e1cb063c0e984a2cafe4778dd7a0c04017718957
areeya tipyasothi <areeya@berkeley.edu>
2026-03-28 00:42:34 -0700

    got rid of some flask specific stuff

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from src.pipeline import *
 from src.cold_score import *
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def home(): 
+@app.route("/api/weather")
+def weather(): 
     weather_data = fetch_weather_data()
     cold_score = weather_data.apply(lambda x: get_cold_score(x.temperature_f), axis=1)
     cold_score_past_hr = cold_score[7]
@@ -17,8 +17,10 @@ def home():
     if cold_score_now == "not cold":
         if_cold = "no"
 
+    '''
     return render_template("index.html", if_cold = if_cold, cold_score = cold_score_now, 
                            last_hr_cold_score = cold_score_past_hr, next_hr_cold_score = cold_score_next_hr)
 
+    '''
 if __name__ == "__main__":
     app.run(debug=True)

9eb844588d92262499a449996f64cfb9821d9df4
areeya tipyasothi <areeya@berkeley.edu>
2026-03-28 00:12:11 -0700

    added more granular cold score logic for more variation (more brrrrrs)

--- src/cold_score.py
+++ src/cold_score.py
@@ -9,13 +9,22 @@ def get_cold_score(temp):
         return "not cold"
     elif temp > 65:
         return "chilly"
+    elif temp > 60:
+        return "soo chilly"
+    elif temp > 55:
+        return "brr"
+    elif temp > 50:
+        return "brrr"
+    elif temp > 45:
+        return "soo brrrr"
     else:
-        return "soo cold"
+        return "brr brr brrrrr"
 
 if __name__ == "__main__":
     weather_data = fetch_weather_data()
     print(weather_data.columns)
-    print(weather_data['temperature_f'])
+    print(weather_data['forecast_time'])
+    temps = weather_data['temperature_f']
 
-    
+    print(weather_data.apply(lambda x: get_cold_score(x.temperature_f), axis=1)[0])  
 

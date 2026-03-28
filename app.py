70bbabbeace1a10e6fd28767ea64eac6e3dfa8c7
areeya tipyasothi <areeya@berkeley.edu>
2026-03-28 00:12:59 -0700

    added more logic in what to display

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home(): 
-    
-    cold_score = "soo cold"
-    return render_template("index.html", if_cold = if_cold, cold_score = cold_score)
+    weather_data = fetch_weather_data()
+    cold_score = weather_data.apply(lambda x: get_cold_score(x.temperature_f), axis=1)
+    cold_score_now = cold_score[8]
+    if_cold = "yes"
+    if cold_score_now == "not cold":
+        if_cold = "no"
+
+    return render_template("index.html", if_cold = if_cold, cold_score = cold_score_now)
 
 if __name__ == "__main__":
     app.run(debug=True)
